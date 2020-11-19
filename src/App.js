66740e6eb4f2a171aa545eb236f56d78735efe2d
alexangria1992 <alex040892@gmail.com>
2020-11-19 14:39:57 +1300

    state tutorial

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TweetList from './components/TweetList';
 import CreateTweet from './components/CreateTweet';
 
 function App() {
-  const name = 'Alex';
-  const message = "I went to sleep today wow."
-
-  //function 
-  const sayHelloHandler = (e) => {
-    console.log(e)
-  }
+  const [name, setName] = useState("alex") 
+  const message  = "Hello"
   return (
     <div>
-      {/* <CreateTweet/>
-      <TweetList name={name} message={message}/> */}
-      <h1>Hello React</h1>
-      <button onClick={sayHelloHandler}>Click</button>
+      <CreateTweet/>
+      <TweetList  name={name} message={message}/>
+
     </div>
   );
 }
--- src/components/CreateTweet.js
+++ src/components/CreateTweet.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const CreateTweet = () => {
+    const[textInput, setTextInput] = useState("")
+    const userInputHandler = (e) => {
+        setTextInput(e.target.value)
+    }
     return(
         <form>
-            <textarea name="" id="" cols="50" rows="5"></textarea>
+            <textarea value={textInput}onChange={userInputHandler} name="" id="" cols="50" rows="5"></textarea>
             <button>Submit</button>
         </form>
 
