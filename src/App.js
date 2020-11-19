9b7d5672b7d68613dead1ec8ab38f943aede7edc
alexangria1992 <alex040892@gmail.com>
2020-11-19 13:28:36 +1300

    props tutorial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import TweetList from './components/TweetList';
+import CreateTweet from './components/CreateTweet';
 
 function App() {
+  const name = 'Alex';
+  const message = "I went to sleep today wow."
   return (
     <div>
-        <h1>Hello</h1>
+      <h1>Hello React</h1>
+      <CreateTweet/>
+      <TweetList name={name} message={message}/>
+
     </div>
   );
 }
---
+++ src/components/CreateTweet.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CreateTweet = () => {
+    return(
+        <form>
+            <textarea name="" id="" cols="50" rows="5"></textarea>
+            <button>Submit</button>
+        </form>
+
+   
+    )
+}
+
+export default CreateTweet;
