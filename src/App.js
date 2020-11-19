eef58eb002d010d0807a4f11d59559bed069079d
alexangria1992 <alex040892@gmail.com>
2020-11-19 13:58:00 +1300

    events tutorial

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ import CreateTweet from './components/CreateTweet';
 function App() {
   const name = 'Alex';
   const message = "I went to sleep today wow."
+
+  //function 
+  const sayHelloHandler = (e) => {
+    console.log(e)
+  }
   return (
     <div>
+      {/* <CreateTweet/>
+      <TweetList name={name} message={message}/> */}
       <h1>Hello React</h1>
-      <CreateTweet/>
-      <TweetList name={name} message={message}/>
-
+      <button onClick={sayHelloHandler}>Click</button>
     </div>
   );
 }
