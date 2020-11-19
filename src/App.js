90eca2ebd3ca7d84a8c9c916c31fff275435acc8
alexangria1992 <alex040892@gmail.com>
2020-11-19 21:42:13 +1300

    rendering multiple components tutorial

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div>
       <CreateTweet textInput={textInput} setTextInput={setTextInput} tweets={tweets} setTweets={setTweets}/>
-      <TweetList  name={name} tweets={tweets}/>
+      <TweetList  name={name} tweets={tweets} setTweets={setTweets}/>
 
     </div>
   );
